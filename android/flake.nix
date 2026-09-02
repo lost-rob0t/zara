3c72bfc97eab77173ba273220c784034247ce4b6
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-02 10:46:04 -0400

    RAGE #172: keep parity toolchain minimal

--- android/flake.nix
+++ android/flake.nix
@@ -95,7 +95,6 @@
               pkgs.gradle_9
               pkgs.gnumake
               pkgs.gcc
-              pkgs.python3
               pkgs.swi-prolog
               androidSdk
             ];
