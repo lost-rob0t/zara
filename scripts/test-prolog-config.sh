d05d7dc401b9463e284104ab079d5f6b9058bbbb
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-03 07:32:44 -0400

    test: make Prolog config RED gate fail closed

--- scripts/test-prolog-config.sh
+++ scripts/test-prolog-config.sh
@@ -10,5 +10,13 @@ mkdir -p "$XDG_CONFIG_HOME/zarathushtra"
 
 swipl -q \
   -s "$repo_root/t/prolog_config.pl" \
+  -g "(run_tests(prolog_config) -> halt(0) ; halt(1))" \
+  -t "halt(1)"
+
+rm -rf "$XDG_CONFIG_HOME/zarathushtra"
+mkdir -p "$XDG_CONFIG_HOME/zarathushtra"
+
+swipl -q \
   -s "$repo_root/t/prolog_config_recovery.pl" \
-  -g "( current_test_unit(prolog_config, _), current_test_unit(prolog_config_recovery, _), current_predicate(config_loader:user_local_config_path/1), run_tests(prolog_config, [summary(BaseSummary)]), get_dict(total, BaseSummary, BaseTotal), BaseTotal > 0, get_dict(failed, BaseSummary, BaseFailed), BaseFailed =:= 0, get_dict(timeout, BaseSummary, BaseTimedOut), BaseTimedOut =:= 0, run_tests(prolog_config_recovery, [summary(RecoverySummary)]), get_dict(total, RecoverySummary, RecoveryTotal), RecoveryTotal > 0, get_dict(failed, RecoverySummary, RecoveryFailed), RecoveryFailed =:= 0, get_dict(timeout, RecoverySummary, RecoveryTimedOut), RecoveryTimedOut =:= 0 -> halt(0) ; halt(1) )"
+  -g "(run_tests(prolog_config_recovery) -> halt(0) ; halt(1))" \
+  -t "halt(1)"
