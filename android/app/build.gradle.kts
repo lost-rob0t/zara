feb6cb7b0e1ed1bfef2cde87ae11963265845979
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-17 00:45:13 -0400

    build(android): add Wear Data Layer client

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -160,5 +160,6 @@ dependencies {
     implementation(libs.compose.material3)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.jeromq)
+    implementation(libs.play.services.wearable)
     testImplementation(libs.junit)
 }
