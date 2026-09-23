1766b257b90dfe00439012ec62503659b4f15461
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-23 08:21:38 -0400

    test(lisp): require bounded server shutdown

--- lisp/t/server-test.lisp
+++ lisp/t/server-test.lisp
@@ -1,5 +1,34 @@
 (in-package :zara/tests)
 
+(test stop-server-bounds-child-shutdown
+  (let ((process
+          (uiop:launch-program
+           '("sh" "-c" "trap '' TERM; sleep 2")
+           :input :null
+           :output :null
+           :error-output :null
+           :wait nil))
+        (server nil))
+    (unwind-protect
+         (progn
+           (setf server
+                 (zara::%make-server
+                  :process process
+                  :endpoint "ipc:///tmp/zara-stop-test.sock"
+                  :runtime-dir (uiop:temporary-directory)
+                  :executable "sh"
+                  :shutdown-timeout 0.05d0))
+           (let ((started (get-internal-real-time)))
+             (zara:stop-server server)
+             (let ((elapsed
+                     (/ (- (get-internal-real-time) started)
+                        (float internal-time-units-per-second 1.0d0))))
+               (is (< elapsed 0.75d0))))
+           (is (not (ignore-errors (uiop:process-alive-p process)))))
+      (when (ignore-errors (uiop:process-alive-p process))
+        (ignore-errors (uiop:terminate-process process :urgent t)))
+      (ignore-errors (uiop:wait-process process)))))
+
 (test real-daemon-handshake
   (when (uiop:getenv "ZARA_LISP_INTEGRATION")
     (zara:with-server (server)
