b44cccfb2cca0217c130fad6e5e83fa6b22a88bc
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-24 20:45:25 -0400

    ci: provide git to coverage push-policy tests

--- flake.nix
+++ flake.nix
@@ -394,7 +394,7 @@
             # writable temp dir so tests that resolve ``Path.home()`` work.
             pytest = pkgs.runCommand "zara-check-pytest"
               {
-                nativeBuildInputs = [ pythonLibs pkgs.swi-prolog pkgs.makeWrapper pkgs.cacert ];
+                nativeBuildInputs = [ pythonLibs pkgs.swi-prolog pkgs.makeWrapper pkgs.cacert pkgs.git ];
                 src = ./.;
               }
               ''
